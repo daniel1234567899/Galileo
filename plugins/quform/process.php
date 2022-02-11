89cb52a2dfc941d30d3764d80a2db4ab943b75c6
daniel1234567899 <daniel169210@gmail.com>
2022-02-11 00:42:53 -0600

    Update process.php

--- plugins/quform/process.php
+++ plugins/quform/process.php
@@ -39,7 +39,7 @@ $config['email'] = true;
  *     'recipient2@example.com'
  * );
  */
-$config['recipients'] = 'a.bublaku@gmail.com';
+$config['recipients'] = 'daniel169210@gmail.com';
 
 /**
  * Set the "From" address of the emails. You should set this to the contact
